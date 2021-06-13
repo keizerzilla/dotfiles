d7d91d3eb3f15c29ffc93c53174d5cedc89ad5dc
Artur Neto <artur.rodrigues26@gmail.com>
2021-06-13 09:31:46 -0300

    pacote xlrd

--- debian_prep.sh
+++ debian_prep.sh
@@ -34,4 +34,4 @@ sudo apt install obs-studio -y
 sudo apt autoremove -y
 
 # Bibliotecas Python
-pip3 install --user numpy scipy pandas matplotlib seaborn scikit-learn scikit-image parse open3d opencv-python spacy kaggle category_encoders jupyterlab youtube_dl optuna pygraphviz
+pip3 install --user numpy scipy pandas matplotlib seaborn scikit-learn scikit-image parse open3d opencv-python spacy kaggle category_encoders jupyterlab youtube_dl optuna pygraphviz xlrd
